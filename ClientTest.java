9e88d4d4a0927a39a31e718290b5029e0da4d502
Yotam124 <44746421+Yotam124@users.noreply.github.com>
2019-09-21 18:17:06 +0300

    Add files via upload

---
+++ ClientTest.java
@@ -0,0 +1,38 @@
+package Gui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ClientTest {
+
+
+	@Test
+	void testClient() {
+		assertTrue(true);
+	}
+	@Test
+	void testMain() {
+		assertTrue(true);
+	}
+
+	@Test
+	void testListenThread() {
+		assertTrue(true);
+	}
+
+	@Test
+	void testSendDisconnect() {
+		assertTrue(true);
+	}
+
+	@Test
+	void testDisconnect() {
+		assertTrue(true);
+	}
+
+}
---
+++ serverTest.java
@@ -0,0 +1,44 @@
+package Gui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class serverTest {
+
+	
+	@Test
+	void testServer() {
+		assertTrue(true);
+	}
+
+	@Test
+	void testMain() {
+		assertTrue(true);
+	}
+
+	@Test
+	void testUserAdd() {
+		assertTrue(true);
+	}
+
+	@Test
+	void testUserRemove() {
+		assertTrue(true);
+	}
+
+	@Test
+	void testSendToAll() {
+		assertTrue(true);
+	}
+
+	@Test
+	void testSendToOne() {
+		assertTrue(true);
+	}
+
+}
